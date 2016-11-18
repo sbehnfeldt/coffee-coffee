786c7a640bd446b4fd1e0e8e1e0bac8e96c4aee1
Stephen Behnfeldt <stephen@stephenbehnfeldt.com>
2016-11-18 12:20:04 -0500

    Improved styling, positining, etc. of post teasers on blog index page

--- src/article-home.php
+++ src/article-home.php
@@ -5,29 +5,25 @@ $diff      = round( ( strtotime( $today ) - strtotime( $published ) ) / ( 24 * 6
 $postClass = $diff < get_option( 'coffee-coffee-age_limit' ) ? 'newArticle' : null;
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( $postClass ); ?>>
+
 	<header class="entry-header">
 		<?php if ( $postClass == 'newArticle' ) : ?>
 			<span class="badge">New!</span>
-			<?php else : ?>
-			<span>&nbsp;</span>
+		<?php endif; ?>
+		<?php if ( has_post_thumbnail() ) : ?>
+			<div class="thumbnail">
+				<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'medium' ); ?></a>
+			</div>
 		<?php endif; ?>
 		<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 		<small>Posted on: <?php the_time( 'F j, Y' ); ?> in <?php echo get_the_category_list( ', ' ); ?></small>
 	</header>
+
 	<div class="row">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<div class="col-xs-12 col-sm-4">
-				<div class="thumbnail"><?php the_post_thumbnail( 'thumbnail' ); ?></div>
-			</div>
-			<div class="col-xs-12 col-sm-8">
-				<?php the_excerpt(); ?>
-				<small><?php echo get_the_tag_list( 'Tagged with ', ', ' ); ?></small>
-			</div>
-		<?php else: ?>
-			<div class="col-xs-12">
-				<?php the_excerpt(); ?>
-				<small><?php echo get_the_tag_list( 'Tagged with ', ', ' ); ?></small>
-			</div>
-		<?php endif; ?>
+		<?php the_excerpt(); ?>
+	</div>
+
+	<div class="footer">
+		<small><?php echo get_the_tag_list( 'Tagged with ', ', ' ); ?></small>
 	</div>
 </article>
--- src/home.php
+++ src/home.php
@@ -17,48 +17,51 @@ endif;
 	<?php get_template_part( 'navbar' ); ?>
 	<img class="page-banner" src="<?php header_image(); ?>" width="100%" alt=""/>
 
-	<h1>Blog Index</h1>
-	<?php $nPosts = get_posts();
-	if ( 0 == $nPosts ) : ?>
-		<div>No Posts Found</div>
-	<?php elseif ( 1 == $nPosts ) :
-		the_post();
-		get_template_part( 'article', 'home' );
-	elseif ( 2 == $nPosts ) : ?>
-		<div class="row">
-			<div class="col-xs-12 col-sm-6">
-				<?php
+	<div class="container">
+		<div id="post-teasers">
+			<?php $nPosts = get_posts();
+			if ( 0 == $nPosts ) : ?>
+				<div>No Posts Found</div>
+			<?php elseif ( 1 == $nPosts ) :
 				the_post();
 				get_template_part( 'article', 'home' );
-				the_post();
-				get_template_part( 'article', 'home' );
-				?>
-			</div>
-		</div>
+			elseif ( 2 == $nPosts ) : ?>
+				<div class="row">
+					<div class="col-xs-12 col-sm-6">
+						<?php
+						the_post();
+						get_template_part( 'article', 'home' );
+						the_post();
+						get_template_part( 'article', 'home' );
+						?>
+					</div>
+				</div>
 
-	<?php else: ?>
-		<?php while ( have_posts() ): ?>
-			<div class="row">
-				<div class="col-xs-12 col-sm-4">
-					<?php the_post();
-					get_template_part( 'article', 'home' );
-					if ( ! have_posts() ): break; endif; ?>
-				</div>
-				<div class="col-xs-12 col-sm-4">
-					<?php the_post();
-					get_template_part( 'article', 'home' );
-					if ( ! have_posts() ): break; endif; ?>
-				</div>
-				<div class="col-xs-12 col-sm-4">
-					<?php the_post();
-					get_template_part( 'article', 'home' ); ?>
-				</div>
-			</div>
-		<?php endwhile; ?>
-	<?php endif; ?>
+			<?php else: ?>
+				<?php while ( have_posts() ): ?>
+					<div class="row">
+						<div class="col-xs-12 col-sm-4">
+							<?php the_post();
+							get_template_part( 'article', 'home' );
+							if ( ! have_posts() ): break; endif; ?>
+						</div>
+						<div class="col-xs-12 col-sm-4">
+							<?php the_post();
+							get_template_part( 'article', 'home' );
+							if ( ! have_posts() ): break; endif; ?>
+						</div>
+						<div class="col-xs-12 col-sm-4">
+							<?php the_post();
+							get_template_part( 'article', 'home' ); ?>
+						</div>
+					</div>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+	</div>
 
 	<?php get_footer(); ?>
-</div>
+</div><!-- /div.container>
 
 </body>
 </html>
