3e65cc3128ad3f0e00ad46f8f4d3187a03f25667
Stephen Behnfeldt <stephen@stephenbehnfeldt.com>
2016-11-28 13:23:29 -0500

    Moved blog index carousel to separate template file.  Added theme option for including blog index carousel.

---
+++ src/blogIndexCarousel.php
@@ -0,0 +1,54 @@
+<!-- Latest blog posts carousel -->
+<div id="latest-posts-carousel" class="carousel slide" data-ride="carousel">
+
+	<?php $indicators = '<li data-target="#latest-posts-carousel" data-slide-to="0" class="active" />'; ?>
+
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner" role="listbox">
+
+		<div class="item active">
+			<img class="page-banner" src="<?php header_image(); ?>" width="100%" alt="" / >
+			<div class="carousel-caption">
+				<h1 class="entry-title"><a href="<?php bloginfo( 'wpurl' ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+				<p><?php bloginfo( 'description' ); ?></p>
+			</div>
+		</div>
+
+		<?php $count = 1;
+		if ( have_posts() ):
+			while ( have_posts() ):
+				the_post();
+				if ( ! has_post_thumbnail() ): continue; endif;
+				?>
+				<!-- ?php get_template_part( 'slider', 'home' ); ? -->
+				<div class="item">
+					<?php the_post_thumbnail( 'full' ); ?>
+					<div class="carousel-caption">
+						<h1><a href="<?php echo the_permalink(); ?>"><?php echo get_the_title(); ?></a></h1>
+						<p><?php echo the_excerpt(); ?></p>
+					</div>
+				</div>
+
+				<?php $indicators .= '<li data-target="#latest-posts-carousel" data-slide-to="' . $count . '" />' ?>
+				<?php $count ++; endwhile; ?>
+			<?php
+		endif;
+		?>
+
+	</div>
+
+	<!-- Indicators -->
+	<ol class="carousel-indicators"><?php echo $indicators; ?></ol>
+
+	<!-- Controls -->
+	<a href="#latest-posts-carousel" class="left carousel-control" role="button" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a href="#latest-posts-carousel" class="right carousel-control" role="button" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right" ariaa-hidden="true"></span>
+		<span class="sr-only">Next</span>
+	</a>
+</div>
+
+
--- src/theme-options.php
+++ src/theme-options.php
@@ -1,25 +1,34 @@
 <?php
-if ( 'POST' ===  $_SERVER['REQUEST_METHOD']) {
-	$ageLimit = $_POST['ageLimit'];
+if ( 'POST' === $_SERVER['REQUEST_METHOD'] ) {
+	$useBlogIndexCarousel = $_POST['useBlogIndexCarousel'];
+	$ageLimit             = $_POST['ageLimit'];
+	update_option( 'coffee-coffee-use_blog_index_carousel', $useBlogIndexCarousel );
 	update_option( 'coffee-coffee-age_limit', $ageLimit );
 	?>
 	<div class="updated">
-		<p><strong><?php _e( 'settings saved.', 'menu-test' ); ?></strong></p>
+		<p><strong><?php _e( 'settings saved.' ); ?></strong></p>
 	</div>
 <?php } else {
-	$ageLimit = get_option( 'coffee-coffee-age_limit');
+	$useBlogIndexCarousel = get_option( 'coffee-coffee-use_blog_index_carousel' );
+	$ageLimit = get_option( 'coffee-coffee-age_limit' );
 }
 ?>
 <div class="wrap">
-	<h2><?php echo __( 'Menu Test Plugin Settings', 'menu-test' ); ?></h2>
+	<h2><?php echo __( 'Menu Test Plugin Settings' ); ?></h2>
 	<form name="optionsForm" method="post" action="">
 		<p>
-			<?php _e( 'New Article Age Limit: ', 'menu-test' ); ?>
+			<?php _e( 'Use blog index carousel: ' ); ?>
+			<input type="checkbox" name="useBlogIndexCarousel" <?php if ( $useBlogIndexCarousel ) : echo ' checked'; endif; ?> />
+		</p>
+
+		<p>
+			<?php _e( 'New Article Age Limit: ' ); ?>
 			<input type="text" name="ageLimit" value="<?php echo $ageLimit; ?>"> days.
 		</p>
 
 		<p class="submit">
-			<input type="submit" name="Submit" class="button button-primary" value="<?php esc_attr_e( 'Save Changes' ) ?>" />
+			<input type="submit" name="Submit" class="button button-primary"
+			       value="<?php esc_attr_e( 'Save Changes' ) ?>"/>
 		</p>
 	</form>
 </div>
