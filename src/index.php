45eec9fd1008a6cf9ee5b0087cbb2ef1efe6a8ec
Stephen Behnfeldt <stephen@stephenbehnfeldt.com>
2017-08-05 11:13:16 -0400

    Removed <h1>index.php</h1> from that template.  Improved display of categories and tags

--- src/index.php
+++ src/index.php
@@ -34,10 +34,9 @@ endif;
 			if ( have_posts() ):
 				while ( have_posts() ): the_post(); ?>
 					<h3><?php the_title(); ?></h3>
-					<h1>index.php</h1>
-					<small>Posted on: <?php the_time( 'F j, Y' ); ?> in <?php the_category(); ?></small>
+					<small>Posted on: <?php the_time( 'F j, Y' ); ?> in <?php the_category( ', '); ?></small>
 					<?php the_content(); ?>
-					<?php the_tags(); ?>
+					<?php the_tags( null, ',' ); ?>
 					<hr/>
 					<?php
 				endwhile;
